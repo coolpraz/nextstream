f247f242d3a52618c6323cdbe2def3304aad7538
Prabhat Shahi <praz.pos@gmail.com>
2024-10-13 10:25:04 +0545

    wip

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
+        $guard = "student";
         return $request->user();
     });
 
